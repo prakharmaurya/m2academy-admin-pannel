da7ef47f2ac1d03ff962b5bcbde50f4aa28f2928
Deepak Kumar Maurya <deepak04122000maurya@gmail.com>
2023-03-27 19:02:49 +0530

    class page called

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/app/Navbar';
 import Protected from './hook/Protected';
+import ForgotPassword from './pages/auth/ForgotPassword';
 import Login from './pages/auth/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Board from './pages/[board]';
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Protected Component={Home} />} />
         <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/forgot_password" element={<ForgotPassword />} />
+        <Route path="/" element={<Protected Component={Home} />} />
+        <Route path="/:id" element={<Protected Component={Board} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
