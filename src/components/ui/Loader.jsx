35aaaf8514cd9196fcf638a7890430f5f0d058ef
Deepak Maurya <deepak04122000maurya@gmial.com>
2024-01-18 12:25:09 +0530

    fix z-index problem

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 bg-black/90 flex items-center justify-center h-fulll">
+    <div className="fixed z-50 inset-0 bg-black/90 flex items-center justify-center h-fulll">
       <span className="loader"></span>
     </div>
   );
